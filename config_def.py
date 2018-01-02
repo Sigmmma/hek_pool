b5f04cb83a1c9cc163c4efe99f57a184d5c1dcd3
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-01-02 01:39:16 -0500

    Starting to become fully functional

--- config_def.py
+++ config_def.py
@@ -1,8 +1,12 @@
 from supyr_struct.defs.tag_def import TagDef
 from supyr_struct.field_types import *
-from .field_widgets import *
-from .constants import *
+from binilla.constants import *
 
+CFG_DIRS = (
+    "working_dir",
+    "last_load_dir",
+    "command_lists_dir",
+    )
 
 def get():
     return config_def
@@ -12,20 +16,26 @@ config_header = Struct("header",
     UEnum32("id", ('Pool', 'looP'), DEFAULT='looP'),
     UInt32("version", DEFAULT=1),
     Bool32("flags",
-        "sync_window_movement",
-        DEFAULT=sum([1<<i for i in (0, 2, 3)]),
+        "open_log",
+        "clear_log",
+        DEFAULT=sum([1<<i for i in (0,)]),
         ),
+    UInt32("max_undos", DEFAULT=1000),
+    Pad(24 - 4*4),
 
     Timestamp32("date_created"),
     Timestamp32("date_modified"),
+
+    UInt16("proc_limit", DEFAULT=1),
+    UInt16("last_tool_index"),
+    Pad(32 - 2*1),
     SIZE=64
     )
 
 array_counts = Struct("array_counts",
-    UInt32("directory_path_count"),
-    UInt32("tool_paths_count"),
-    UInt32("tool_cmd_list_count"),
-    SIZE=64,
+    UInt16("directory_path_count"),
+    UInt16("tool_paths_count"),
+    SIZE=16,
     )
 
 app_window = Struct("app_window",
@@ -33,7 +43,7 @@ app_window = Struct("app_window",
     UInt16("app_height", DEFAULT=300),
     SInt16("app_offset_x"),
     SInt16("app_offset_y"),
-    SIZE=64,
+    SIZE=16,
     )
 
 filepath = Container("filepath",
@@ -41,23 +51,13 @@ filepath = Container("filepath",
     StrUtf8("path", SIZE=".path_len")
     )
 
-tool_command_list = Container("tool_command_list",
-    UInt32("name_len"),
-    UInt32("cmd_len"),
-    StrUtf8("name",     SIZE=".name_len"),
-    StrUtf8("commands", SIZE=".cmd_len"),
-    )
-
 config_def = TagDef("hek_pool_config",
     config_header,
     array_counts,
     app_window,
     Array("directory_paths", SUB_STRUCT=filepath,
-        SIZE=".array_counts.directory_path_count",
-        NAME_MAP=("last_load_dir", "curr_dir", "debug_log_path",)),
+        SIZE=".array_counts.directory_path_count", NAME_MAP=CFG_DIRS),
     Array("tool_paths", SUB_STRUCT=filepath,
         SIZE=".array_counts.tool_paths_count"),
-    Array("tool_commands", SUB_STRUCT=tool_command_list,
-        SIZE=".array_counts.tool_cmd_list_count"),
     ENDIAN='<', ext=".cfg",
     )
