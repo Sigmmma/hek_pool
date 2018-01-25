64f02569d9201b4e254f3c7339f54c0478db53e4
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-01-25 00:44:36 -0500

    version bump

--- hek_pool/setup.py
+++ hek_pool/setup.py
@@ -9,7 +9,7 @@ curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
 release_date = "2018.01.24"
-version = (1, 1, 0)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+version = (1, 1, 1)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:
