3b43f1c42a84927863ba2ed3ff29d65dc1f29eca
gbMichelle <gbmichelle.dev@gmail.com>
2020-02-14 01:54:38 +0100

    Bump version

--- hek_pool/__init__.py
+++ hek_pool/__init__.py
@@ -4,5 +4,5 @@
 __author__ = "Devin Bobadilla, Michelle van der Graaf"
 #           YYYY.MM.DD
 __date__ = "2020.01.24"
-__version__ = (1, 4, 1)
+__version__ = (1, 4, 2)
 __website__ = "https://github.com/MosesofEgypt/hek_pool"
